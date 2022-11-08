f6229bca4658b16a2996388d4bb0f1a0ef7cfc38
Jeba-anika <anikanur98@gmail.com>
2022-11-08 17:24:55 +0600

    general setup done

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ app.use(cors())
 app.use(express.json())
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7tamy9s.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
 app.get('/', (req, res)=>{
     res.send('HematoCare server is running')
 })
