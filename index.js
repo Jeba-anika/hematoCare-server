980e3d65620e645404a2f4100c558aec61cba078
Jeba-anika <anikanur98@gmail.com>
2022-11-09 00:50:30 +0600

    services route created

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express =require('express')
 const cors = require('cors')
 const app = express();
 require('dotenv').config()
+const jwt = require('jsonwebtoken')
 const port = process.env.PORT || 5000;
 
 
@@ -14,6 +15,53 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7tamy9s.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function VerifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        res.status(401).send({message: 'Unauthorized Access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            res.status(403).send({message: 'Unauthorized Access'})
+        }
+        req.decoded =decoded
+        next()
+    })
+}
+
+async function run(){
+    try{
+        const servicesCollection = client.db('hematoCare').collection('services') 
+
+
+        app.post('/jwt', (req, res)=>{
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            })
+            res.send({token})
+        })
+
+        app.get('/home', async(req, res)=>{
+            const query = {};
+            const cursor = servicesCollection.find(query)
+            const services = await cursor.limit(3).toArray();
+            res.send(services)
+        })
+
+        app.get('/services', async(req, res)=>{
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+    }finally{
+
+    }
+}
+
+run().catch(err => console.log(err))
 
 
 
