3c68b7e9b3ba0162c4c9aff3507bbb9ad7be1527
Jeba-anika <anikanur98@gmail.com>
2022-11-09 01:05:28 +0600

    each service route created

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7tamy9s.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -55,6 +55,13 @@ async function run(){
             const cursor = servicesCollection.find(query);
             const services = await cursor.toArray();
             res.send(services)
+        })
+
+        app.get('/services/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = { _id: ObjectId(id)}
+            const service = await servicesCollection.findOne(query);
+            res.send(service)
         })
     }finally{
 
